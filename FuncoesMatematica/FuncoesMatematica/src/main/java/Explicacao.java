7fd6b6ba840966b0c2773d24422410131666275d
Arthur de Oliveira Tomé <arthuroliveira2815@gmail.com>
2026-02-25 23:32:11 -0300

    Fuções Matemática em Java

---
+++ FuncoesMatematica/FuncoesMatematica/src/main/java/Explicacao.java
@@ -0,0 +1,7 @@
+void main (){
+    /*Algumas funções matemáticas em Java
+    *  Exemplos:
+    * A = Math.sqrt(x);   Significado: Váriavel A recebe a raiz quadrada de x
+    * A = Math.pow(x, y);  Significado: Váriavel A recebe o resultado de x elevado a y
+    * A = Math.abs(x);    Significado: Váriavel A recebe o valor absoluto de x*/
+}
