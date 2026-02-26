e57c1dd6f7c989fc25f6e57d3a53e0ae93fbefe4
Arthur de Oliveira Tomé <arthuroliveira2815@gmail.com>
2026-02-26 17:40:29 -0300

    Termino dos exercicios

--- Exercicios/Exercicios/src/main/java/Exercicio4.java
+++ Exercicios/Exercicios/src/main/java/Exercicio4.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
-void main(){
-    
-    Scanner sc = new Scanner(System.in);
 
+void main() {
+
+    Scanner sc = new Scanner(System.in);
+    int Id, HorasTrabalhadas;
+    double ValorHora, Salario;
+
+    System.out.println("Informe o Id: ");
+    Id = sc.nextInt();
+
+    System.out.println("Informe as Horas Trabalhadas: ");
+    HorasTrabalhadas = sc.nextInt();
+
+    System.out.println("Informe o valor da hora: ");
+    ValorHora = sc.nextDouble();
+
+    Salario = HorasTrabalhadas * ValorHora;
+
+    System.out.printf("O salario do trabalhador Id: %d é de R$ %.2f%n", Id, Salario);
+
+    sc.close();
 }
