94d9b0e2329bffd0a755ffb77e8fc66d2f1c2bb2
Arthur de Oliveira Tomé <arthuroliveira2815@gmail.com>
2026-02-28 15:38:57 -0300

    Operador de atribuição acumulativa

---
+++ src/main/java/Operadores.java
@@ -0,0 +1,10 @@
+void main(){
+
+    /*Operadores de atribuição cumulativa
+    *  a+= b;       a = a + b;
+    *  a -=b;       a = a- b;
+    *  a *= b;      a= a * b;
+    *  a /= b;      a = a / b;
+    *  a %= b;      a = a % b; */
+
+}
